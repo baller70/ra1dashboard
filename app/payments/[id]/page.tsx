1b4e6f1aa587744650509b519072b33d95d53761
baller70 <135007227+baller70@users.noreply.github.com>
2025-08-08 10:12:54 -0400

    ♻️ Disable Next.js caching on payments page (force-dynamic, revalidate 0) to ensure Vercel shows latest UI

--- app/payments/[id]/page.tsx
+++ app/payments/[id]/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { useState, useEffect } from 'react'
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 import { useParams, useRouter } from 'next/navigation'
 import Link from 'next/link'
 import { Button } from '../../../components/ui/button'
